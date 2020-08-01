9f305ef20ecda902df2dbb7012184aa39fcef183
lathief <latifmukhtarul@ce.student.pens.ac.id>
2020-08-02 01:13:10 +0700

    add jump and gravity flappybird

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ document.addEventListener("DOMContentLoaded",() =>{
     const bird = document.querySelector('.bird');
     const gameDisplay = document.querySelector('.game-container');
     const ground = document.querySelector('.ground');
+
+    let birdLeft=270
+    let birdBottom=100
+    let gravity= 2
+
+    function startGame(){
+        //birdBottom-=gravity
+        bird.style.bottom=birdBottom+'px'
+        bird.style.left=birdLeft+'px'
+    }
+    let timerId=setInterval(startGame,20)
+
+    function jump(){
+        birdBottom+=50
+        bird.style.bottom=birdBottom+'px'
+    }
+
+    document.addEventListener('keyup',jump)
 })
